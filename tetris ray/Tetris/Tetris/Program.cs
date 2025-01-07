5abc7df119b3e64abeb25e70af8f421a83862819
Onyxia84 <raphcaron75@gmail.com>
2025-01-07 05:23:32 +0100

    resize and center

--- tetris ray/Tetris/Tetris/Program.cs
+++ tetris ray/Tetris/Tetris/Program.cs
@@ -3,35 +3,19 @@ using Raylib_cs;
 
 namespace Tetris
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             Raylib.SetConfigFlags(ConfigFlags.ResizableWindow);
-            Raylib.InitWindow(800, 480, "Hello World");
+            Raylib.InitWindow(800, 480, "Tetris");
+
+            Grid grid = new Grid(); // Création d'une instance de Grid
 
             while (!Raylib.WindowShouldClose())
             {
-                Raylib.BeginDrawing();
-                Raylib.ClearBackground(Color.White);
-
-                Raylib.DrawRectangle(275, 200, 100, 200, Color.Blue);
-
-                for (int i = 285; i < 375;) {
-                    Raylib.DrawLine(i, 200, i, 400, Color.Black);
-                    i += 10;
-                }
-                for (int j = 210; j < 400;)
-                {
-                    Raylib.DrawLine(275, j, 375, j, Color.Black);
-                    j += 10;
-                }
-                
-
-                Raylib.EndDrawing();
+                grid.GridDraw(); // Appel via l'instance
             }
-
             Raylib.CloseWindow();
         }
     }
